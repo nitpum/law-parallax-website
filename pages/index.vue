cac9006cf1f7f27133d10725f93c6e77f9b40b4d
nitpum <nitpum@outlook.com>
2020-01-20 00:39:05 +0700

    Add home img and vue-scrollmagic

--- pages/index.vue
+++ pages/index.vue
@@ -1,44 +1,25 @@
 <template>
   <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        law-parallax-website
-      </h1>
-      <h2 class="subtitle">
-        Website
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
+    <div id="header">
+      <div id="title">
+        <img src="/imgs/home/title.png" />
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-
 export default {
-  components: {
-    Logo
+  mounted() {
+    const header = this.$scrollmagic.scene({
+      triggerElement: '#header',
+      triggerHook: 0
+    })
   }
 }
 </script>
 
-<style>
+<style lang="scss">
 .container {
   margin: 0 auto;
   min-height: 100vh;
@@ -48,25 +29,10 @@ export default {
   text-align: center;
 }
 
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
+#header {
+  background-image: url('/imgs/home/bg.png');
+  background-size: cover;
+  width: 100%;
+  height: 100vh;
 }
 </style>
