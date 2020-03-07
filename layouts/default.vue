720318aa60055fcec4f48218d63653770f9aaedf
nitpum <nitpum@outlook.com>
2020-03-07 13:13:53 +0700

    Add cursor

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,6 +5,9 @@
 </template>
 
 <style>
+* {
+  cursor: url('/imgs/cursor.png'), auto;
+}
 html {
   font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
     Roboto, 'Helvetica Neue', Arial, sans-serif;
